cecde08ac597348939c312f0fc35d6c9401e8475
mukesh mahla <mukesh.23it647@rtu.ac.in>
2025-09-20 01:06:52 +0530

    some change

--- apps/excaildarw/next.config.ts
+++ apps/excaildarw/next.config.ts
@@ -3,10 +3,7 @@ import path from "path";
 
 
 const nextConfig: NextConfig = {
-   webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, './'); // makes @ point to app root
-    return config;
-  },
+   
 };
 
 export default nextConfig;

cebfac3dd7692c578710ac7a71af79e4bd6b1d15
mukesh mahla <mukesh.23it647@rtu.ac.in>
2025-09-21 14:58:32 +0530

    n

--- apps/excaildarw/app/canvas/[roomId]/page.tsx
+++ apps/excaildarw/app/canvas/[roomId]/page.tsx
@@ -3,7 +3,7 @@ import { Canvas } from "@/component/canvas";
 type Props = {
   params: { roomId: string };
   searchParams?: { [key: string]: string | string[] | undefined };
-};
+}
 
 export default function CanvasPage({ params }: Props) {
   const { roomId } = params;

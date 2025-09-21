9e9f6aaac6dbea5ed0499f58769d23a763830896
mukesh mahla <mukesh.23it647@rtu.ac.in>
2025-09-21 14:17:50 +0530

    setting up frontend

--- apps/excaildarw/component/authPage.tsx
+++ apps/excaildarw/component/authPage.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/link"
+
 import { useRef } from "react"
 
 
--- apps/excaildarw/next.config.ts
+++ apps/excaildarw/next.config.ts
@@ -3,10 +3,7 @@ import path from "path";
 
 
 const nextConfig: NextConfig = {
-   webpack(config) {
-    config.resolve.alias["@"] = path.resolve(__dirname, "./");
-    return config;
-   }
+  
 };
 
 export default nextConfig;

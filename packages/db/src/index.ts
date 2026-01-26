0a74225cd053c8122bda024623ca88c5d7d67836
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-01-26 21:59:14 +0530

    fixing db provider

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as {
   prisma?: PrismaClient;
@@ -7,6 +8,9 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    adapter: new PrismaNeon({
+      connectionString: process.env.DATABASE_URL!,
+    }),
     log: ["error"],
   });
 

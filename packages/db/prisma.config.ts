679a090caf7682437f966aa97d1f303c3df83470
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-01-26 22:40:58 +0530

    ..

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 const DATABASE_URL = process.env.DATABASE_URL;
@@ -6,5 +7,6 @@ console.log("DATABASE_URL =", DATABASE_URL);
 export default defineConfig({
   datasource: {
     url: DATABASE_URL,
+    
   },
 });

037ef3aae03ec8ed9f4af041b4ad7a04fab29370
mukesh mahla <mukesh.23it647@rtu.ac.in>
2026-01-26 20:13:16 +0530

    fixing prisma issue

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { error } from "node:console";
 
 const globalForPrisma = globalThis as any
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient();
+  new PrismaClient({
+    log: ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
